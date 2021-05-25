ca4bc7331eba94931bd19ab2e92548622b28d1e5
Philippe AURIOU <p.auriou@gmail.com>
2021-05-25 08:36:00 +0200

    fix init controls

---
+++ src/CommunityToolkit/Xamarin.CommunityToolkit/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.CommunityToolkit
+{
+	public static class Initializer
+	{
+		public static void Init()
+		{
+		}
+	}
+}
